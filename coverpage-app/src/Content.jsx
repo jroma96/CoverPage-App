e52ac2d8b8b19ee3be8762226633ad61578d2ae7
jroma96 <31935178+jroma96@users.noreply.github.com>
2026-02-04 16:19:00 -0600

    Ajustes de estilo y creacion de skill list
    Se redistribuyo el espacio en el sidebar de manera que la imagen no desperdicie espacio y se agrego una lista de skills y se creo la logica para renderizar dicha lista de manera que los elementos ocupen varias lineas.

--- coverpage-app/src/Content.jsx
+++ coverpage-app/src/Content.jsx
@@ -12,10 +12,10 @@ function Content() {
 function Main() {
   return (
     <div className="main">
-      <div className="educacion">
+      <div className="section">
         <h2>Educacion</h2>
         <div style={{ display: "flex", flexDirection: "row", gap: "25px" }}>
-          <h3>Universidad Rafael Landivar</h3>
+          <h3 style={{ marginLeft: "35px" }}>Universidad Rafael Landivar</h3>
           <img src="https://www.url.edu.gt/ave/logoURL.jpg" />
         </div>
         <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
@@ -23,7 +23,43 @@ function Main() {
           <img src="https://miro.medium.com/v2/resize:fit:1400/0*7VyEZgzwUhQMeBqb" />
         </div>
       </div>
-      <div></div>
+      <div className="section">
+        <h2>Experiencia Laboral</h2>
+        <div style={{ display: "grid", gridTemplateRows: "1fr 1fr" }}>
+          <div>
+            <h3 style={{ textDecoration: "underline", justifySelf: "center" }}>
+              Business Development Group – Guatemala (Remoto)
+            </h3>
+            <h4>Software Developer</h4>
+            <p>
+              Desarrollo y mantenimiento de servicios backend y soluciones
+              automatizadas utilizando JavaScript, .NET y SQL. Diseño e
+              implementación de procesos ETL para integración y transformación
+              de datos entre sistemas. Construcción de APIs y servicios backend
+              integrados con bases de datos relacionales. Optimización de
+              consultas SQL y mejora de performance en procesos críticos.
+              Implementación de pruebas unitarias y end-to-end para asegurar
+              calidad y estabilidad. Integración de soluciones con servicios en
+              la nube (AWS). Colaboración activa con equipos multidisciplinarios
+              bajo metodologías ágiles.
+            </p>
+          </div>
+          <div>
+            <h3 style={{ textDecoration: "underline", justifySelf: "center" }}>
+              Capgemini Technology Services – Guatemala
+            </h3>
+            <h4>Junior Software Develeper</h4>
+            <p>
+              Desarrollo de soluciones backend y automatización utilizando
+              principios de programación orientada a objetos. Participación
+              activa en code reviews y mejora continua del código. Colaboración
+              con equipos cross-functional para implementación de soluciones
+              técnicas. Análisis y mejora de procesos relacionados con bases de
+              datos y lógica de negocio.
+            </p>
+          </div>
+        </div>
+      </div>
       <div></div>
     </div>
   );
--- coverpage-app/src/SideBar.jsx
+++ coverpage-app/src/SideBar.jsx
@@ -1,12 +1,55 @@
 import profile from "./assets/profile-pic.jpeg";
 
-const skills = ["Javascript", "React.js", "SQL", "Node.js", "Azure", "AWS", ""];
+const skills = [
+  "Javascript",
+  "React.js",
+  "SQL",
+  "Node.js",
+  "Azure Cloud Services",
+  "Azure Pipelines",
+  "AWS",
+  "C#",
+  ".Net Framework",
+  "Vite",
+  "CSS",
+  "HTML5",
+  "NOSQL",
+  "MongoDB",
+  "CosmoDB",
+  "Agile",
+];
 
 function SideBar() {
   return (
     <div className="sidebar">
       <img src={profile} />
-      <div className="container"></div>
+      <div>
+        <h3
+          style={{
+            textDecoration: "underline",
+            fontSize: "3vw",
+            color: "white",
+          }}
+        >
+          Habilidades
+        </h3>
+        <ul
+          style={{
+            display: "flex",
+            alignItems: "flex-start",
+            flexWrap: "wrap",
+            gap: "10px",
+            marginBlockStart: "0px",
+            paddingInlineStart: "0px",
+            paddingLeft: "10px",
+            paddingRight: "10px",
+          }}
+        >
+          {skills.map((skill) => (
+            <Skill>{skill}</Skill>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
